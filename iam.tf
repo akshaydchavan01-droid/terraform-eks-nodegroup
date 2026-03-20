772e32fa96e4d46afe354a703817518d08b184e4
akshaydchavan01-droid <akshaydchavan01@gmail.com>
2026-03-20 13:53:20 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -33,7 +33,22 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.example.name
 }
-
+resource "aws_iam_role_policy_attachment" "AmazonEKSBlockStoragePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicy"
+  role       = aws_iam_role.example.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSComputePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSComputePolicy"
+  role       = aws_iam_role.example.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSLoadBalancingPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+  role       = aws_iam_role.example.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSNetworkingPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy"
+  role       = aws_iam_role.example.name
+}
 # Optionally, enable Security Groups for Pods
 # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
