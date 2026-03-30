0123c093c0bc54dc9c2bd4728e3cceb0f4467802
akshaydchavan01-droid <akshaydchavan01@gmail.com>
2026-03-30 15:38:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
 # -------------------------------
 # EKS CLUSTER
 # -------------------------------
-resource "aws_eks_cluster" "ankit_cluster" {
+resource "aws_eks_cluster" "ankit_cluster-v02" {
   name     = "ankit-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
